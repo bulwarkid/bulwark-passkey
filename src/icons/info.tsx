c392f0fb385f29a6e4f0a7ddd8296011201c1ee5
Chris de la Iglesia <chris@cmdli.dev>
2022-09-15 15:22:58 -0700

    Add settings and info icons

---
+++ src/icons/info.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export class InfoIcon extends React.Component {
+    render() {
+        return (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-info"
+            >
+                <circle cx="12" cy="12" r="10"></circle>
+                <line x1="12" y1="16" x2="12" y2="12"></line>
+                <line x1="12" y1="8" x2="12.01" y2="8"></line>
+            </svg>
+        );
+    }
+}
