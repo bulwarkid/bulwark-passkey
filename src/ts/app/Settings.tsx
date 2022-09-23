7bc0443042d89ee9cc4345de097bca6e50f16702
Chris de la Iglesia <chris@cmdli.dev>
2022-09-23 15:36:54 -0700

    Refactor TS code into separate folder

--- src/ts/app/Settings.tsx
+++ src/ts/app/Settings.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { ListItem, ListSection } from "../components/List";
-import { app, App } from "./App";
 import { PassphraseModal } from "./modals/Passphrase";
 import * as modal from "./ModalContainer";
 
 export class Settings extends React.Component {
     render() {
         return (
-            <div className="flex flex-col w-full py-8">
+            <div className="w-full py-8">
                 <ListSection title="Settings">
                     <ListItem
                         text="Passphrase"
