8684dbb8d0f1222f0230502fde85b03aaab46048
Chris de la Iglesia <chris@cmdli.dev>
2022-09-23 16:03:00 -0700

    Convert Settings title bar into flat text title

--- src/ts/app/Settings.tsx
+++ src/ts/app/Settings.tsx
@@ -7,9 +7,11 @@ import { TitleBar } from "../components/TitleBar";
 export class Settings extends React.Component {
     render() {
         return (
-            <div className="w-full">
-                <TitleBar title="Settings" />
-                <div className="pt-8">
+            <div className="w-full pt-4">
+                <div className="flex justify-center">
+                    <span className="text-xl font-bold">Settings</span>
+                </div>
+                <div className="pt-4">
                     <ListSection>
                         <ListItem
                             text="Passphrase"
