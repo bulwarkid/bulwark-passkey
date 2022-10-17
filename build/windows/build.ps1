e15cc1adaec8d3f752f5034da56bb431423aecbb
Chris de la Iglesia <chris@cmdli.dev>
2022-10-17 16:39:43 -0700

    Fail Powershell when build stages fail

--- build/windows/build.ps1
+++ build/windows/build.ps1
@@ -1,12 +1,18 @@
 
 Push-Location .\frontend
 npm run build
+if (-not $?) {
+    throw "Frontend build failure"
+}
 Pop-Location
 
 Copy-Item -Path .\frontend\build -Destination .\app\frontend_dist -Recurse
 
 Push-Location .\app
 wails build -s -skipbindings -debug
+if (-not $?) {
+    throw "App build failure"
+}
 Pop-Location
 
 New-Item .\output -ItemType Directory
