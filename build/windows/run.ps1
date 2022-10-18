c62f705b7487be8e89e59ec0e3b3325e945a7e50
Chris de la Iglesia <chris@cmdli.dev>
2022-10-18 13:54:51 -0700

    Print out log lines to console

--- build/windows/run.ps1
+++ build/windows/run.ps1
@@ -1,4 +1,8 @@
 
 Push-Location .\output
-.\bulwark_passkey.exe
+if (-not (Test-Path -Path .\log.txt)) {
+    New-Item -Path .\log.txt -ItemType File
+}
+Start-Process .\bulwark_passkey.exe
+Get-Content -Path .\log.txt -Wait
 Pop-Location
