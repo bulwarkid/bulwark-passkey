8fac164502c8a002dd380d691576e6e36cf1a810
Chris de la Iglesia <chris@cmdli.dev>
2022-11-07 13:01:24 -0800

    Convert About modal to new style

--- frontend/src/ts/app/modals/About.tsx
+++ frontend/src/ts/app/modals/About.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Modal } from "../../components/Modal";
+import { Button, ButtonColor, ButtonSize } from "../../components/Buttons";
+import { CardModal, CardModalTitle, Modal } from "../../components/Modal";
 import { TitleBar, TitleBarButton } from "../../components/TitleBar";
 
 type AboutModalProps = {
@@ -9,33 +10,36 @@ type AboutModalProps = {
 export class AboutModal extends React.Component<AboutModalProps> {
     render() {
         const title = (
-            <TitleBar
+            <CardModalTitle
                 title="About"
-                rightButton={
-                    <TitleBarButton
-                        onClick={this.props.onCancel}
+                button={
+                    <Button
                         text="Close"
+                        onClick={this.props.onCancel}
+                        color={ButtonColor.CLEAR}
+                        size={ButtonSize.SM}
                     />
                 }
             />
         );
+        const content = (
+            <div className="grow flex flex-col items-center justify-center p-4">
+                <img src="/img/logo.png" width={100} />
+                <p className="font-bold text-2xl">Bulwark Passkey</p>
+                <p className="text-sm">
+                    Made by BulwarkID, Inc. in Oakland, CA
+                </p>
+                <p className="text-sm">Copyright 2022</p>
+                <a className="underline text-sm" href="mailto:corp@bulwark.id">
+                    Contact Us
+                </a>
+            </div>
+        );
         return (
-            <Modal title={title}>
-                <div className="grow flex flex-col items-center justify-center">
-                    <img src="/img/logo.png" width={150} />
-                    <p className="font-bold text-2xl">Bulwark Passkey</p>
-                    <p className="text-sm">
-                        Made by BulwarkID, Inc. in Oakland
-                    </p>
-                    <p className="text-sm">Copyright 2022</p>
-                    <a
-                        className="underline text-sm"
-                        href="mailto:corp@bulwark.id"
-                    >
-                        Contact Us
-                    </a>
-                </div>
-            </Modal>
+            <CardModal>
+                {title}
+                {content}
+            </CardModal>
         );
     }
 }
