e6b338e0423549bdd2a55bd3a6003c057583c908
Chris de la Iglesia <chris@cmdli.dev>
2022-11-29 14:08:47 -0800

    Update marketing website with new Passkey-focused copy

--- marketing-next/pages/_cta.tsx
+++ marketing-next/pages/_cta.tsx
@@ -1,19 +1,19 @@
+import DownloadLink from "./_download_link";
+
 export default function CallToAction() {
     return (
         <div className="bg-white">
-            <div className="mx-auto max-w-7xl py-12 px-4 text-center sm:px-6 lg:py-16 lg:px-8">
+            <div className="mx-auto max-w-7xl py-12 px-4 text-center sm:px-6 lg:py-16 lg:px-8 flex flex-col items-center">
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                     <span className="block">Want to try out passkeys?</span>
                     <span className="block">Download it today.</span>
                 </h2>
-                <div className="mt-8 flex justify-center">
-                    <div className="inline-flex rounded-md shadow">
-                        <a
-                            href="#"
-                            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700"
-                        >
-                            Download
-                        </a>
+                <div className="flex justify-center mt-5 max-w-md sm:flex md:mt-8 space-x-2">
+                    <div className="rounded-md shadow">
+                        <DownloadLink source="windows" />
+                    </div>
+                    <div className="rounded-md shadow">
+                        <DownloadLink source="linux" />
                     </div>
                 </div>
             </div>
--- marketing-next/pages/_hero.tsx
+++ marketing-next/pages/_hero.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { WindowsLogoIcon } from "@fluentui/react-icons-mdl2-branded";
 import DownloadLink from "./_download_link";
 
 export default function HeroSection() {
@@ -8,14 +7,14 @@ export default function HeroSection() {
             <div className="lg:grid lg:grid-cols-12 lg:gap-8">
                 <div className="lg:col-span-6 flex flex-col justify-center mx-12">
                     <p className="text-lg font-semibold text-indigo-600">
-                        Now in Alpha!
+                        Now in Beta!
                     </p>
                     <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                         <span className="block xl:inline">Bulwark Passkey</span>
                     </h1>
                     <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
-                        A simple, software-based FIDO device for Two Factor
-                        Authentication and WebAuthN.
+                        An open-source passkey manager for the future of
+                        passwordless logins.
                     </p>
                     <div className="mt-5 max-w-md sm:flex md:mt-8 space-x-2">
                         <div className="rounded-md shadow">
