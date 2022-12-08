c0fb591b6173859817b5705b1c02a2334f51e86d
Chris de la Iglesia <chris@cmdli.dev>
2022-12-08 12:20:20 -0800

    Remove broken 'Contact Us' link

--- frontend/src/ts/app/modals/About.tsx
+++ frontend/src/ts/app/modals/About.tsx
@@ -30,9 +30,6 @@ export class AboutModal extends React.Component<AboutModalProps> {
                     Made by BulwarkID, Inc. in Oakland, CA
                 </p>
                 <p className="text-sm">Copyright 2022</p>
-                <a className="underline text-sm" href="mailto:corp@bulwark.id">
-                    Contact Us
-                </a>
             </div>
         );
         return (
