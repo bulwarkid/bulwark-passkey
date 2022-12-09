f4ef48c6af254f2b9023953837f50bc7e43c5c34
Chris de la Iglesia <chris@cmdli.dev>
2022-12-09 11:43:11 -0800

    Fix Tailwind not seeing new content folder

--- marketing-next/tailwind.config.js
+++ marketing-next/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
         "./app/**/*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
+        "./content/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},
