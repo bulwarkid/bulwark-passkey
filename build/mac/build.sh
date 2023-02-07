151f8e5e52410a75e69d2e44e1c1b8df08b7cbdb
Chris de la Iglesia <chris@cmdli.dev>
2023-02-06 20:09:48 -0800

    Enable darwin/universal build on Mac

--- build/mac/build.sh
+++ build/mac/build.sh
@@ -13,14 +13,14 @@ CONFIG_DIR="build/darwin"
 APP_PATH="build/bin/Bulwark Passkey.app"
 # TODO: Don't just use local app certificate in keychain for signing
 APP_CERTIFICATE_NAME="E191A2286FBE73BD20E7417959C7A4F429B0C4C5" 
-wails build -s -skipbindings -debug
+wails build -s -skipbindings -platform darwin/universal -debug
 mkdir -p "$APP_PATH/Contents/Library/SystemExtensions" && cp -r "$CONFIG_DIR/id.bulwark.VirtualUSBDriver.driver.dext" "$APP_PATH/Contents/Library/SystemExtensions"
 mkdir -p "$APP_PATH/Contents/Frameworks" && cp "mac/installer/libinstaller.dylib" "$APP_PATH/Contents/Frameworks"
 cp "$CONFIG_DIR/profile.provisionprofile" "$APP_PATH/Contents/embedded.provisionprofile"
 # TODO: See if we can fix the libinstaller.dylib load path before this step
 install_name_tool -change libinstaller.dylib @rpath/libinstaller.dylib "$APP_PATH/Contents/MacOS/Bulwark Passkey"
 install_name_tool -add_rpath @loader_path/../Frameworks "$APP_PATH/Contents/MacOS/Bulwark Passkey"
-codesign --force --sign "$APP_CERTIFICATE_NAME" --options=runtime --entitlements "$CONFIG_DIR/app.entitlements" --timestamp=none --verbose --generate-entitlement-der "$APP_PATH"
+codesign --force --deep --sign "$APP_CERTIFICATE_NAME" --options=runtime --entitlements "$CONFIG_DIR/app.entitlements" --timestamp=none --verbose --generate-entitlement-der "$APP_PATH"
 cd ..
 
 mkdir -p output && mv "app/$APP_PATH" output/
